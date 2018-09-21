9bcc242741f7a015555008e94b2517bc0155bd76
wq1019 <2013855675@qq.com>
2018-09-22 02:03:54 +0800

    update

--- index.php
+++ index.php
@@ -6,27 +6,28 @@ use Proto\HelloRequest;
 require __DIR__ . '/vendor/autoload.php';
 
 //用于连接 服务端
+
 try {
     $client = new Client('127.0.0.1:50052', [
         'credentials' => Grpc\ChannelCredentials::createInsecure()
     ]);
 } catch (Exception $e) {
+    die($e->getMessage());
 }
 
-//实例化 TestRequest 请求类
+
 $request = new HelloRequest();
 $request->setName("sunlong");
 
-//调用远程服务
-$get = $client->SayHello($request)->wait();
-
-//返回数组
-//$reply 是 HelloReply 对象
-//$status 是数组
-list($reply, $status) = $get;
-
-if ($status->code == 14) {
-    throw new Exception("error:" . $status->details);
+//调用远程服务,返回数组
+$response = $client->SayHello($request)->wait();
+list($reply, $status) = $response;
+/**
+ * @var stdClass $status
+ * @var \Proto\HelloReply $reply
+ */
+if ($status->code == Grpc\STATUS_UNAVAILABLE) {
+    die("error:" . $status->details . PHP_EOL);
 }
-$getdata = $reply->getMessage();
-print_r($getdata);
+$data = $reply->getMessage();
+print_r($data);
